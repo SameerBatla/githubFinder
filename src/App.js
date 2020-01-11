0bc92bdbad02a9b2661fd159e3fa4d801fd75c88
SameerBatla <sambatla456@gmail.com>
2020-01-11 17:36:10 +0530

    added the clear functionality

--- src/App.js
+++ src/App.js
@@ -14,27 +14,50 @@ export class App extends Component {
     users: [],
     loading: false
   };
-  async componentDidMount() {
-    console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+  // async componentDidMount() {
+  //   // console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+  //   this.setState({
+  //     loading: true
+  //   });
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+  //   this.setState({
+  //     users: res.data,
+  //     loading: false
+  //   });
+  // }
+  searchUsers = async text => {
     this.setState({
       loading: true
     });
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
     this.setState({
-      users: res.data,
+      users: res.data.items,
       loading: false
     });
-  }
+  };
+  clearUsers = () => {
+    this.setState({
+      users: [],
+      loading: false
+    });
+  };
   render() {
+    const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar />
         {/* <UserItem /> */}
         <div className="container">
-          <Search />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
--- src/Components/Users/Search.js
+++ src/Components/Users/Search.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
   state = {
     text: ''
   };
 
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
+  };
+
   onChange = e => {
     this.setState({
-      text: e.target.value
+      [e.target.name]: e.target.value
     });
   };
   onSubmit = e => {
     e.preventDefault();
-    console.log(this.state.text);
+    this.props.searchUsers(this.state.text);
+    this.setState({
+      text: ''
+    });
+    // console.log(this.state.text);
   };
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form className="form" onSubmit={this.onSubmit}>
@@ -32,6 +44,11 @@ export class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
