636ac0fe999520e93dcb71c1eb58110dc3f84d0b
SameerBatla <sambatla456@gmail.com>
2020-01-11 14:01:16 +0530

    setting up individual user

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Navbar from './Components/Layout/Navbar';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+// import UserItem from './Components/Layout/Users/UserItem';
+import Users from './Components/Layout/Users/Users';
 library.add(fab);
 
 export class App extends Component {
@@ -10,6 +12,10 @@ export class App extends Component {
     return (
       <div className="App">
         <Navbar />
+        {/* <UserItem /> */}
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
