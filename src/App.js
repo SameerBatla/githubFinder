e2ac47abde8389cfce16f520f94f2e3c15b7d07b
SameerBatla <sambatla456@gmail.com>
2020-01-12 18:22:16 +0530

    adding routing which displays users info

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './Components/Layout/Navbar';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 // import UserItem from './Components/Layout/Users/UserItem';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import Users from './Components/Users/Users';
+import User from './Components/Users/User';
 import axios from 'axios';
 import Search from './Components/Users/Search';
 import { Alert } from './Components/Layout/Alert';
 import { About } from './Components/Pages/About';
 
-library.add(fab, faInfoCircle);
+library.add(fab, faInfoCircle, faCheck, faTimesCircle);
 
 export class App extends Component {
   state = {
     users: [],
     loading: false,
+    user: {},
     alert: null
   };
   // async componentDidMount() {
@@ -45,6 +49,21 @@ export class App extends Component {
       loading: false
     });
   };
+
+  //single github user
+
+  getUser = async username => {
+    this.setState({
+      loading: true
+    });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({
+      user: res.data,
+      loading: false
+    });
+  };
   clearUsers = () => {
     this.setState({
       users: [],
@@ -59,7 +78,7 @@ export class App extends Component {
     setTimeout(() => this.setState({ alert: null }), 3000);
   };
   render() {
-    const { users, loading } = this.state;
+    const { users, user, loading } = this.state;
     return (
       <Router>
         <div className="App">
@@ -84,6 +103,18 @@ export class App extends Component {
                 )}
               />
               <Route exact path="/about" component={About} />
+              <Route
+                exact
+                path="/user/:login"
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>
         </div>
